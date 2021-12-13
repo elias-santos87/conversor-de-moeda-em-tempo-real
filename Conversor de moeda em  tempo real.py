4f7f29bd34c60d1cb3401cf13ea86adde7060368
Elias Santos <48299934+elias-santos87@users.noreply.github.com>
2021-12-13 05:41:19 -0300

    Delete Conversor de moeda em  tempo real.py

--- Conversor de moeda em  tempo real.py
+++
@@ -1,9 +0,0 @@
-
-from forex_python.converter import CurrencyRates
-c = CurrencyRates()
-amount = int(input('Entre com o amount:'))
-from_currency = input('From Currency:').upper()
-to_currency = input('To Currency:').upper()
-print(from_currency, 'To', to_currency, amount)
-resultado = c.convert(from_currency, to_currency, amount)
-print(resultado)
